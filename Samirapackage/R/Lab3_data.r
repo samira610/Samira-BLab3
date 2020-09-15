353a67df7410650c05e17ba7f72548d6928e6af6
unknown <samiraee@gmail.com>
2020-09-15 21:38:54 +0200

    update

---
+++ Samirapackage/R/Lab3_data.r
@@ -0,0 +1,12 @@
+#' dataframe wiki_graph
+#' 
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{v1}{wiki_graph}
+#'   \item{v2}{wiki_graph}
+#'   \item{w}{weight of the wiki_graph}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"wiki_graph"

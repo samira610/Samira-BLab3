2709cfcc9b373f7872571684e98d698b06cb2050
unknown <samiraee@gmail.com>
2020-09-21 16:13:10 +0200

    euclidean final

--- Samirapackage/R/Lab_3_euclidean.R
+++ Samirapackage/R/Lab_3_euclidean.R
@@ -1,6 +1,7 @@
 #' @title euclidean
 #' @details greatest common divisor two numbers by euclidean algorithm.
-#' @description (a,b)=(b,r) if a=b*q+r and r is remaining.
+#' @description In this algorithm, the quotient and remainder are found by repeated subtraction, until the last remainder that is grater than zero. 
+#' Euclidean division reduces all the steps between two exchanges into a single step.
 #' @param a A number.
 #' @param b A number.
 #' @return Greatest common divisor of a and b \code{a} and \code{b}.
